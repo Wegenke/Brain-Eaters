73a0b042b0768a915da9d57b987011777586a45f
Matthew Wegenke <matthew.wegenke@gmail.com>
2016-12-07 19:53:50 -0600

    Just figuring things out

--- braineaters.js
+++ braineaters.js
@@ -1,12 +1,72 @@
 var canvas = document.getElementById("myCanvas");
-var context = canvas.getContext("2d");
-var img = new Image();
-img.src = "zombie.gif";
-window.addEventListener("load", function () {
-    context.drawImage(img, 0, 0);
-});
-var mainLoop = function () {
-    updateGame();
-    drawGame();
+var ctx = canvas.getContext("2d");
+var img = new Image(35, 35);
+img.src = "mario.jpg";
+var keyInput = new KeyboardInput();
+var x = 50;
+var y = 50;
+function gameLoop() {
+    requestAnimationFrame(gameLoop);
+    keyInput.inputLoop();
+    ctx.fillStyle = "#26034d";
+    ctx.fillRect(0, 0, 900, 700);
+    ctx.save();
+    ctx.translate(x, y);
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+    ctx.restore();
+}
+var KeyboardInput = (function () {
+    function KeyboardInput() {
+        var _this = this;
+        this.keyCallback = {};
+        this.keyDown = {};
+        this.addKeycodeCallback = function (keycode, f) {
+            _this.keyCallback[keycode] = f;
+            _this.keyDown[keycode] = false;
+        };
+        this.keyboardDown = function (event) {
+            event.preventDefault();
+            _this.keyDown[event.keyCode] = true;
+        };
+        this.keyboardUp = function (event) {
+            _this.keyDown[event.keyCode] = false;
+        };
+        this.inputLoop = function () {
+            for (var key in _this.keyDown) {
+                var is_down = _this.keyDown[key];
+                if (is_down) {
+                    var callback = _this.keyCallback[key];
+                    if (callback != null) {
+                        callback();
+                    }
+                }
+            }
+        };
+        document.addEventListener("keydown", this.keyboardDown);
+        document.addEventListener("keyup", this.keyboardUp);
+    }
+    return KeyboardInput;
+}());
+function marioUp() {
+    y -= 2;
+}
+function marioDown() {
+    y += 2;
+}
+function marioLeft() {
+    x -= 2;
+}
+function marioRight() {
+    x += 2;
+}
+window.onload = function () {
+    keyInput.addKeycodeCallback(37, marioLeft);
+    keyInput.addKeycodeCallback(65, marioLeft);
+    keyInput.addKeycodeCallback(38, marioUp);
+    keyInput.addKeycodeCallback(87, marioUp);
+    keyInput.addKeycodeCallback(39, marioRight);
+    keyInput.addKeycodeCallback(68, marioRight);
+    keyInput.addKeycodeCallback(40, marioDown);
+    keyInput.addKeycodeCallback(83, marioDown);
+    gameLoop();
 };
-setInterval(mainLoop, ONE_FRAME_TIME);
--- braineaters.ts
+++ braineaters.ts
@@ -1,13 +1,92 @@
-let canvas = document.getElementById("myCanvas");
-let context = canvas.getContext("2d");
-let img = new Image();
-img.src = "zombie.gif";
-window.addEventListener("load", () => {
-      context.drawImage(img, 0, 0);
-  });
+let canvas = <HTMLCanvasElement>document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+let img = new Image(35, 35);
+img.src = "mario.jpg";
 
-let mainLoop = function(){
-  updateGame();
-  drawGame();
+let keyInput = new KeyboardInput();
+let x: number = 50;
+let y: number = 50;
+
+function gameLoop(): void {
+  requestAnimationFrame(gameLoop);
+  keyInput.inputLoop();
+
+  ctx.fillStyle = "#26034d";
+  ctx.fillRect(0, 0, 900, 700);
+
+  ctx.save();
+  ctx.translate(x, y);
+  ctx.drawImage(img, 0, 0, img.width, img.height);
+  ctx.restore();
+}
+
+class KeyboardInput {
+   public keyCallback: { [keycode: number]: () => void; } = {};
+   public keyDown: { [keycode: number]: boolean; } = {};
+
+   constructor() {
+      document.addEventListener("keydown", this.keyboardDown);
+      document.addEventListener("keyup", this.keyboardUp);
+   }
+
+    public addKeycodeCallback = (keycode: number, f: () => void): void => {
+      this.keyCallback[keycode] = f;
+      this.keyDown[keycode] = false;
+    }
+
+   public keyboardDown = (event: KeyboardEvent): void => {
+      event.preventDefault();
+      this.keyDown[event.keyCode] = true;
+   }
+   public keyboardUp = (event: KeyboardEvent): void => {
+      this.keyDown[event.keyCode] = false;
+   }
+   public inputLoop = (): void => {
+      for (let key in this.keyDown) {
+          let is_down: boolean = this.keyDown[key];
+
+          if (is_down) {
+              let callback: () => void = this.keyCallback[key];
+              if (callback != null) {
+                  callback();
+              }
+          }
+      }
+  }
+}
+
+function marioUp(): void {
+  y -= 2;
+}
+
+function marioDown(): void {
+  y += 2;
+}
+
+function marioLeft(): void {
+  x -= 2;
+}
+
+function marioRight(): void {
+  x += 2;
+}
+
+window.onload = () => {
+   // PRESS LEFT ARROW OR 'A' KEY
+   keyInput.addKeycodeCallback(37, marioLeft);
+   keyInput.addKeycodeCallback(65, marioLeft);
+
+   // PRESS UP ARROW OR 'W' KEY
+   keyInput.addKeycodeCallback(38, marioUp);
+   keyInput.addKeycodeCallback(87, marioUp);
+
+   // PRESS RIGHT ARROW OR 'D' KEY
+   keyInput.addKeycodeCallback(39, marioRight);
+   keyInput.addKeycodeCallback(68, marioRight);
+
+   // PRESS DOWN ARROW OR 'S' KEY
+   keyInput.addKeycodeCallback(40, marioDown);
+   keyInput.addKeycodeCallback(83, marioDown);
+
+   gameLoop();
 };
-setInterval(mainLoop, ONE_FRAME_TIME);
